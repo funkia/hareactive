4eb241f1b8e908fcd4dd74bcb6a6202a22ea581b
Emil Gjørup <limemloh@gmail.com>
2016-06-07 17:31:36 +0200

    Removal of old unused code and examples tsconfig

--- src/behaviour/stepper.js
+++
@@ -1,9 +0,0 @@
-var h = require('../frp-common.js');
-var B = h.Behavior;
-var E = h.Event;
-
-module.exports = function(init, e) {
-  var b = B.BehaviorK(init);
-  e.eventListeners.push(b);
-  return b;
-};
--- src/frp-common.ts
+++ src/frp-common.ts
@@ -18,23 +18,3 @@ export type SubscribeFunction<A> = ((a: A) => void);
 export type PushFunction<A> = ((a: A) => any);
 export type ScanFunction<A, B> = ((b: B, a: A) => B);
 export type FilterFunction<A> = ((a: A) => boolean);
-
-export class MapBody<A, B> implements Body {
-  private fn: MapFunction<A, B>;
-  private source: Reactive<A>;  // srcE
-  private target: Reactive<B>;   // ev
-
-  constructor(fn: MapFunction<A, B>, target: Reactive<B>, source: Reactive<A>) {
-    this.fn = fn;
-    this.target = target;
-    this.source = source;
-  }
-
-  public run: ((a: A) => void) = a => {
-    this.target.publish(this.fn(a));
-  }
-
-  public pull: (() => B) = () => {
-    return this.fn(((this.source.last !== undefined) ? this.source.last : this.source.body.pull()));
-  }
-}
