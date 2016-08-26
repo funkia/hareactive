a3527c07532100f3a73d4b9922f783513492765c
paldepind <simonfv@gmail.com>
2016-08-26 10:33:52 +0200

    Add working example

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,14 +1,48 @@
-import {Component, h, input, br} from "../../src/DOMBuilder";
-import {mount, declareBehaviors} from "../../src/bootstrap";
+import {Do} from "jabz/monad";
 
-function app(): Component {
-  const [name] = declareBehaviors("");
-  return h("div", [
-    h("span.classname#myid", ["Hello "]), h("span", [name]),
-    br(),
-    h("label", ["Name: "]),
-    {inputValue: name.def} = input()
-  ]);
-}
+import {Behavior} from "../../src/Behavior";
+import {Stream} from "../../src/Stream";
+import {Now} from "../../src/Now";
 
-mount("body", app);
+import {
+  Component, component, runMain, span, input, br, text, viewOut
+} from "../../src/framework";
+
+const isValidEmail = (s: string) => s.match(/.+@.+\..+/i);
+
+const getLength = (s: string) => s.length;
+
+type Model = {
+  validB: Behavior<boolean>,
+  lengthB: Behavior<number>
+};
+
+type ViewBehaviors = [Behavior<string>];
+type ViewStreams = Stream<any>[];
+
+const main = component<Model, ViewBehaviors, ViewStreams>({
+  model({behaviors: [emailB]}: {behaviors: Behavior<string>[]}) {
+    return Do(function*(): Iterable<Now<any>> {
+      return Now.of({
+        validB: emailB.map(isValidEmail),
+        lengthB: emailB.map(getLength)
+      });
+    });
+  },
+  view({validB, lengthB}) {
+    return Do(function*(): Iterable<Component<any>> {
+      yield span("Please enter an email address:")
+      const {inputValue: emailB} = yield input();
+      yield br();
+      yield text("The length of the email is "),
+      yield text(lengthB),
+      yield br();
+      yield text("The address is ");
+      yield text(validB.map(t => t ? "valid" : "invalid"));
+      return viewOut([emailB], []);
+    });
+  }
+});
+
+// `runMain` should be the only impure function in application code
+runMain("body", main);
