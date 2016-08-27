3dc9a62f428976cbbafe06f018993ed3d1c20de6
paldepind <simonfv@gmail.com>
2016-08-27 14:05:00 +0200

    Add simple counters example

---
+++ examples/counters/index.ts
@@ -0,0 +1,71 @@
+import {Do} from "jabz/monad";
+
+import {Behavior, stepper} from "../../src/Behavior";
+import * as B from "../../src/Behavior";
+import {Stream, snapshotWith, scan, merge, map} from "../../src/Stream";
+import {Now} from "../../src/Now";
+
+import {
+  Component, component, runMain, span, input, br, text, button, div
+} from "../../src/framework";
+
+// Counter
+
+type CounterModelOut = {
+  count: Behavior<number>
+};
+
+type CounterViewOut = {
+  streams: [Stream<any>, Stream<any>]
+};
+
+const counter = component<CounterModelOut, CounterViewOut>({
+  model({streams: [incrementClick, decrementClick]}) {
+    return Do(function*(): Iterator<Now<any>> {
+      const increment = map(_ => 1, incrementClick);
+      const decrement = map(_ => -1, decrementClick);
+      const count = B.stepper(
+        0,
+        scan((n, m) => Math.max(n + m, 0), 0, merge(increment, decrement))
+      );
+      return Now.of({count});
+    });
+  },
+  view: ({count}) =>
+    div(Do(function*() {
+      yield text("Counter ");
+      yield text(count);
+      yield text(" ");
+      const {click: increment} = yield button(" + ");
+      yield text(" ");
+      const {click: decrement} = yield button(" - ");
+      yield br;
+      return Component.of({streams: [increment, decrement]})
+    }))
+});
+
+const isValidEmail = (s: string) => s.match(/.+@.+\..+/i);
+
+const getLength = (_: any, s: string) => s.length;
+
+type MainModel = {};
+
+type MainViewOut = {};
+
+const main = component<MainModel, MainViewOut>({
+  model({}) {
+    return Do(function*(): Iterator<Now<any>> {
+      return Now.of({});
+    });
+  },
+  view({}) {
+    return Do(function*(): Iterator<Component<any>> {
+      yield counter;
+      yield counter;
+      return Component.of({behaviors: [], streams: []});
+    });
+  }
+});
+
+// `runMain` should be the only impure function in application code
+runMain("body", main);
