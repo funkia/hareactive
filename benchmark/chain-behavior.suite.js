15191e22ed0e5f177fe38dd856ae9770f84a70ef
paldepind <simonfv@gmail.com>
2016-09-07 11:28:31 +0200

    Add two benchmarks for behavior

---
+++ benchmark/chain-behavior.suite.js
@@ -0,0 +1,68 @@
+var Suite = require("./default-suite").Suite;
+var most = require("most");
+var S = require("../dist/Stream");
+var B = require("../dist/Behavior");
+var Bo = require("./hareactive-old/dist/Behavior");
+var So = require("./hareactive-old/dist/Stream");
+
+var n = 100000;
+
+module.exports = Suite("chain-behavior")
+
+  .add("Old behavior", function(defered) {
+    var b1 = Bo.sink(true);
+    var b2 = Bo.sink(1);
+    var b3 = Bo.sink(2);
+    var b = b1.chain((b) => b ? b2 : b3);
+    Bo.subscribe(function(e) {
+      if (e === n - 1) {
+        defered.resolve();
+      }
+    }, b);
+    for (var i = 0; i <= n; ++i) {
+      switch (i % 4) {
+      case 0:
+        b1.publish(false);
+        break;
+      case 1:
+        b2.publish(i);
+        break;
+      case 2:
+        b1.publish(true);
+        break;
+      case 3:
+        b3.publish(i);
+        break;
+      }
+    }
+  }, {defer: true})
+
+  .add("Behavior", function(defered) {
+    var b1 = B.sink(true);
+    var b2 = B.sink(1);
+    var b3 = B.sink(2);
+    var b = b1.chain((b) => b ? b2 : b3);
+    B.subscribe(function(e) {
+      if (e === n - 1) {
+        defered.resolve();
+      }
+    }, b);
+    for (var i = 0; i <= n; ++i) {
+      switch (i % 4) {
+      case 0:
+        b1.publish(false);
+        break;
+      case 1:
+        b2.publish(i);
+        break;
+      case 2:
+        b1.publish(true);
+        break;
+      case 3:
+        b3.publish(i);
+        break;
+      }
+    }
+  }, {defer: true})
+
+  .run({"async": true});
---
+++ benchmark/map-map-map-behavior.suite.js
@@ -0,0 +1,57 @@
+var Suite = require("./default-suite").Suite;
+var most = require("most");
+var S = require("../dist/Stream");
+var B = require("../dist/Behavior");
+var Bo = require("./hareactive-old/dist/Behavior");
+var So = require("./hareactive-old/dist/Stream");
+
+var n = 100000;
+var testData = [];
+
+for (var i = 0; i < n; i++) {
+  testData[i] = i;
+}
+
+function add1(n) {
+  return n + 1;
+}
+
+function double(n) {
+  return n * 2;
+}
+
+function sub3(n) {
+  return n - 3;
+}
+
+var result = sub3(double(add1(n - 1)));
+
+function pushArray(arr, ev) {
+  for (var i = 0; i < arr.length; ++i) {
+    ev.publish(arr[i]);
+  }
+}
+
+module.exports = Suite("map-map-map-behavior")
+
+  .add("Behavior old", function(defered) {
+    var b = Bo.sink();
+    B.subscribe(function(e) {
+      if (e === result) {
+        defered.resolve();
+      }
+    }, b.map(add1).map(double).map(sub3));
+    pushArray(testData, b);
+  }, {defer: true})
+
+  .add("Behavior", function(defered) {
+    var b = B.sink();
+    B.subscribe(function(e) {
+      if (e === result) {
+        defered.resolve();
+      }
+    }, b.map(add1).map(double).map(sub3));
+    pushArray(testData, b);
+  }, {defer: true})
+
+  .run({"async": true});
